5e0a6023e69e0cd5a48386d798cec9c4bfe98f70
vinod <get2vinodtk@gmail.com>
2025-03-19 17:16:26 +0530

    Fetch API and Routing example

---
+++ Projects/fetch_demo/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Navigate to Posts</h1>
+{#each {length: 10}, i}
+  <a href="/posts/{i+1}">Post-{i+1}</a>
+  <br />
+{/each}
+<br /><br /><br />
+<h1>Navigate to Recipes</h1>
+{#each {length: 10}, i}
+  <a href="/recipes/{i+1}">Recipe-{i+1}</a>
+  <br />
+{/each}
+
+
---
+++ Projects/fetch_demo/src/routes/recipes/[recipeid]/+page.ts
@@ -0,0 +1,14 @@
+import type { PageLoad } from "../$types";
+
+export const load: PageLoad = async ({ params }) => {
+  const response = await fetch(
+    `https://dummyjson.com/recipes/${params.recipeid}`
+  );
+  const recipe = await response.json();
+  console.log(recipe);
+  return {
+    name: recipe.name,
+    ingredients: recipe.ingredients,
+    instructions: recipe.instructions
+  };
+};
